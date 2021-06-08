e3382f189f01aad73e0e163ce19cb9126abdddc4
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-08 08:13:37 +0100

    alert, prompt and confiram

---
+++ 02_Interaction.js
@@ -0,0 +1,14 @@
+"use strict"
+
+alert
+alert("Hello")
+
+let age = prompt('How old are you?', 100);
+alert(`You are ${age} years old`);
+
+
+let isBoss = confirm("Are you the boss?");
+alert(isBoss);
+
+let name = prompt("What is your name?");
+alert(`My name is ${name}`);
