66ce5dc0ae201bbd4f81ed8b308d6494c4541fb2
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-08 17:17:03 +0100

    this keyword

---
+++ 10_this.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let user = {
+    name: 'Manas', 
+    age: 30,
+    sayHi(){
+        console.log("i need " + this.name);
+    }
+}
+
+// user.sayHi = function() {
+//     console.log('hello!');
+// }
+
+// user.sayHi
+
+// function sayHi(){
+//     console.log('hi!');
+// }
+
+// user.sayHi = sayHi;
+// user.sayHi()
