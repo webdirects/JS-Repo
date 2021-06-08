6d32687946226559d9b60b8bd14fb89e27c893bc
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-08 01:09:34 +0100

    Variables

--- 01_var.js
+++ 01_var.js
@@ -1,3 +1,13 @@
 "use strict"
 
 let message;
+
+message = "Hello";
+console.log(message);
+
+let user = 'John', age = 25, msg = 'hello'
+console.log(user, age, msg);
+
+// constant
+const my = '18.04.1982';
+console.log(my);
