7942964e534948ab28f431d58267a59d8f6d4325
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-08 09:59:35 +0100

    switch statement

---
+++ 07_switch.js
@@ -0,0 +1,15 @@
+let a = 2 + 2
+
+switch(a) {
+    case 3:
+        alert('Too Small');
+        break;
+    case 4:
+        alert('Exactly');
+        break;
+    case 5:
+        alert('Too big');
+        break;
+    default:
+        alert('I dont know it')
+}
