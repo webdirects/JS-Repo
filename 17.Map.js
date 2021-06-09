86a121053139fd940f81d9c570fbf36ad8b3729c
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-09 14:20:42 +0100

    maps

---
+++ 17.Map.js
@@ -0,0 +1,12 @@
+
+let map = new Map(); // creates the map.
+
+map.set('1', 'str1');
+map.set(1, 'num1');
+map.set(true, 'boll');
+
+console.log(map);
+
+console.log(map.get(1));
+
+console.log(map.size);
